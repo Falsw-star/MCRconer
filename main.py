458189cff6af495a5c2f43e54d525a83b2cdf49b
Falsw-star <Falsestar114514@outlook.com>
2025-01-19 09:58:55 +0800

    JsonText, title and playsound

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import mcrcon # type: ignore
-import os, json
+import json
 import re
 
+__version__ = "0.1.0"
 
 CLIENT: 'Rcon' = None # type: ignore
-
 
 class Rcon(mcrcon.MCRcon):
     def __init__(self, host, password, port=25575, tlsmode=0, timeout=5):
@@ -17,6 +17,37 @@ class Rcon(mcrcon.MCRcon):
         global CLIENT
         CLIENT = None
         return super().disconnect()
+
+class JsonText(object):
+    def __init__(self, text: str = "",
+            color: str = "white",
+            font: str = "",
+            bold: bool = False,
+            italic: bool = False,
+            underlined: bool = False,
+            strikethrough: bool = False,
+            obfuscated: bool = False) -> None:
+        self.color = color
+        self.font = font
+        self.bold = bold
+        self.italic = italic
+        self.underlined = underlined
+        self.strikethrough = strikethrough
+        self.obfuscated = obfuscated
+        self.text = text
+    
+    def __str__(self) -> str:
+        content: dict = {
+            "text": self.text,
+            "color": self.color
+        }
+        if self.font: content["font"] = self.font
+        if self.bold: content["bold"] = self.bold
+        if self.italic: content["italic"] = self.italic
+        if self.underlined: content["underlined"] = self.underlined
+        if self.strikethrough: content["strikethrough"] = self.strikethrough
+        if self.obfuscated: content["obfuscated"] = self.obfuscated
+        return json.dumps(content, ensure_ascii=False)
 
 def _call(command: str) -> str:
     if not CLIENT: raise ValueError("Please connect to server first.")
@@ -34,42 +65,53 @@ def list_players() -> list:
     return players
 
 class tellraw(object):
-    def __init__(self, message: str = "", target: str = "@a", 
-            color: str = "white",
-            font: str = "",
-            bold: bool = False,
-            italic: bool = False,
-            underlined: bool = False,
-            strikethrough: bool = False,
-            obfuscated: bool = False) -> None:
+    def __init__(self, message: JsonText | None = None, target: str = "@a") -> None:
         """If message is not empty, method __init__ will send it.
         Use method send to send message to server within returned server's response."""
+        self.message = message
         self.target = target
-        self.color = color
-        self.font = font
-        self.bold = bold
-        self.italic = italic
-        self.underlined = underlined
-        self.strikethrough = strikethrough
-        self.obfuscated = obfuscated
         if message: self.send(message, target)
         
-    def send(self, message: str, target: str = "") -> str:
+    def send(self, message: JsonText, target: str = "") -> str:
         if not target: target = self.target
-        content: dict = {
-            "text": message,
-            "color": self.color
-        }
-        if self.font: content["font"] = self.font
-        if self.bold: content["bold"] = self.bold
-        if self.italic: content["italic"] = self.italic
-        if self.underlined: content["underlined"] = self.underlined
-        if self.strikethrough: content["strikethrough"] = self.strikethrough
-        if self.obfuscated: content["obfuscated"] = self.obfuscated
-        command = f"tellraw {target} {json.dumps(content, ensure_ascii=False)}"
+        command = f"tellraw {target} {message.__str__()}"
         return _call(command)
 
-class ban(object):
+class title(object):
+    def __init__(self, target: str = "@a", fade_in: float = 0.5, stay: float = 3.5, fade_out: float = 1.0) -> None:
+        """Method __init__ will not send meassage to server.
+        Fades in, stays, and fades out are in seconds."""
+        self.target = target
+        self.fade_in: int = round(fade_in * 20)
+        self.stay: int = round(stay * 20)
+        self.fade_out: int = round(fade_out * 20)
+    
+    def title(self, content: JsonText, target: str = "") -> str:
+        if not target: target = self.target
+        command = f"title @a times {self.fade_in} {self.stay} {self.fade_out} title {content.__str__()}"
+        return _call(command)
+    def subtitle(self, content: JsonText, target: str = "") -> str:
+        if not target: target = self.target
+        command = f"title @a times {self.fade_in} {self.stay} {self.fade_out} subtitle {content.__str__()}"
+        return _call(command)
+    def actionbar(self, content: JsonText, target: str = "") -> str:
+        if not target: target = self.target
+        command = f"title @a times {self.fade_in} {self.stay} {self.fade_out} actionbar {content.__str__()}"
+        return _call(command)
+
+def playsound(sound: str,
+              source: str = "master",
+              target: str = "@a",
+              pos: tuple[float, float, float] = (0, 0, 0),
+              volume: float = 1.0,
+              pitch: float = 1.0) -> str:
+    """Method playsound will play sound to server.
+    Check https://minecraft.fandom.com/zh/wiki/Sounds.json for more sound names."""
+    x, y, z = pos
+    command = f"playsound {sound} {source} {target} {x} {y} {z} {volume} {pitch}"
+    return _call(command)
+
+class banner(object):
     def __init__(self, player: str = "") -> None:
         """Method __init__ will not send command to server.
         Use method ban or pardon."""
